1ef9d41668306a2d680fda384d31853a814c6ac8
Matvey <59504782+mml20@users.noreply.github.com>
2021-03-14 10:31:16 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 function next(){
-    document.location.href = "../screens/menu.html";
+    document.location.href = "https://mml20.github.io/game_town.github.io/screens/menu.html";
     localStorage.coin = 0;
 };
