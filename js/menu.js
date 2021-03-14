025729267cbed426bd7d9219148057ef3a87bdc2
Matvey <59504782+mml20@users.noreply.github.com>
2021-03-14 10:32:20 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 let coin_text = document.getElementById("coin_text");
 coin_text.innerHTML = parseInt(localStorage.coin) + "$";
 function level(lvl){
-    document.location.href = "../screens/level"+lvl+".html";
+    document.location.href = "https://mml20.github.io/game_town.github.io/screens/level"+lvl+".html";
 }
